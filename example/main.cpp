a86d1bb5673996d5f31470d22b5c03cb3b6f687f
Mike <lubinetsm@yandex.ru>
2016-03-03 21:23:54 +0300

    Added macro defining VK API methods returning value for future changes

--- example/main.cpp
+++ example/main.cpp
@@ -18,35 +18,6 @@ using namespace vk;
 #define PASSWORD       "theunpredictableFortunes"
 
 int main(int argc, char** argv) {
-    /// Logger can be configured with initial values on definition
-        /// so as in runtime
-        log::print_timer  = true;
-        log::print_func   = true;
-        log::print_level  = true;
-        log::print_source = false;
-
-        /// Set maximum verbosity level
-        log::log_level    = log::trace;
-
-        /// Use clog as errors and warning output stream instead of default cerr
-        log::err_ostream = clog;
-
-
-
-        /// Go logging!
-        ERROR()   << "Error message";
-        WARNING() << "Some warning";
-        INFO()    << "%username%, do you know that big loging library sucks?";
-        LOG()     << "Informed user";
-        LOG1()    << "Printint log message about logging on log level 1";
-        LOG2()    << "And another about log level 2";
-        LOG3()    << "Now shit got boring";
-        TRACE()   << "Finally the last loglevel to make your disk hurt with gigabytes of logs";
-
-        return 0;
-/*
-
-
     srand(clock());
 
     log::print_timer = true;
@@ -92,6 +63,5 @@ int main(int argc, char** argv) {
     //args["message"] = "Ублюдок, мать твою, а ну иди сюда, говно собачье, а ну решил ко мне лезть, ты... засранец вонючий, мать твою. А?!";
     //api.messages.send(args);
 
-*/
     return 0;
 }
